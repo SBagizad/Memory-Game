66a08edac0c51e3147570567fe9a0967c29a773e
Shira Bagit <shira.bagit@gmail.com>
2022-04-05 20:14:16 +0300

    adding cards style

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
-const cards= ["A", "B", "c","A", "B", "c" ]
-const board = document.getElementById("board")
-for (i of cards){
-const element = document.createElement("div")
-element.innerHTML= i;
-board.appendChild(element);
+const cards = ["A", "B", "C", "A", "B", "C"]
+
+function createCard(idx){
+    const cardEl=document.createElement("div")
+    cardEl.innerHTML=cards[idx];
+    cardEl.id=idx
+    cardEl.className= "cards"
+
+    return cardEl
 }
+
+function shuffle (arr){
+    arr.sort(() => Math.random() - 0.5);
+ }
+
+shuffle(cards);
+
+const board = document.getElementById("board")
+ for (i in cards){
+    const element = createCard(i)
+    board.appendChild(element)
+ }
