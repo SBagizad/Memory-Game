f9801d7c51ffd9e4bcba07286813ca9bd6104403
Shira Bagit <shira.bagit@gmail.com>
2022-04-12 18:29:02 +0300

    adding an image

--- main.js
+++ main.js
@@ -1,22 +1,32 @@
-const cards = ["A", "B", "C", "A", "B", "C"]
+const cards = ["😃", "🥳", "🥰 ", "🤩", "😇", "😴", " 🤗", " 😜", "😃", "🥳", "🥰", "🤩", "😇", "😴", " 🤗", " 😜"]
+const card2= 
+function init() {              //מאתחל את הקוד את הלוח 
+    shuffle(cards); //  ועושה ערבוב קוראת למערך
+    const board = document.getElementById("board"); // קורא לאלמנט
+    for (i in cards) {// עבור כל קלף ,כך תא קוראת לפונקציה של create card
+        const element = createCard(i)
+        board.appendChild(element)
+    }
 
-function createCard(idx){
-    const cardEl=document.createElement("div")
-    cardEl.innerHTML=cards[idx];
-    cardEl.id=idx
-    cardEl.className= "cards"
+}
+init();//קריאה לפנוקציה והקלפים  עולים
+
+function createCard(idx) {//  שזה בעצם עבור כל קלףמכניסים את האינדקס 
+    const cardEl = document.createElement("div")
+    cardEl.innerHTML='<img src=" >'
+    cardEl.style.cursor = "pointer";
+    // cardEl.innerHTML=cards[idx];// זה התוכן של הקלף (אימוג'י)
+    cardEl.addEventListener("click", element => cardEl.innerHTML = cards[idx]); // מראה של התוכן
+    cardEl.id = idx// ID של כל קלף
+    cardEl.className = "cards col-6 col-md-4 col-lg-2", // פריסה של הקלפים על המסך 
 
     return cardEl
 }
 
-function shuffle (arr){
+function shuffle(arr) {
     arr.sort(() => Math.random() - 0.5);
- }
+}
 
-shuffle(cards);
 
-const board = document.getElementById("board")
- for (i in cards){
-    const element = createCard(i)
-    board.appendChild(element)
- }
+
+
