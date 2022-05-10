8c7fe39661c11bc0f1a377853b5d409c75d3502f
Shira Bagit <shira.bagit@gmail.com>
2022-05-10 10:33:36 +0300

    improvement

--- main.js
+++ main.js
@@ -1,25 +1,104 @@
 const cards = ["😃", "🥳", "🥰 ", "🤩", "😇", "😴", " 🤗", " 😜", "😃", "🥳", "🥰", "🤩", "😇", "😴", " 🤗", " 😜"]
-const card2= 
+let openCards = []
+let cnt = 0;
+let turn = 0;
+
+let player1 = document.getElementById("player1");
+let player2 = document.getElementById("player2");
+// const body = document.querySelector("body");
+// let playerOne = document.createElement("div");
+// let playerTwo = document.createElement("div");
+// body.append(playerOne, playerTwo);
+// playerTwo.className = "example";
+// playerTwo.innerText = "How it look?";
+
+// player1.innerText = `name:${players[0].name} score: ${players[0].score} `
+// player2.innerText = `name:${players[1].name} score: ${players[1].score} `
+
+
+let name1 = prompt("Please insert Player 1 Name:")
+let name2 = prompt("Please insert Player 2 Name:")
+let players = [createPlayer(name1), createPlayer(name2)];
+
+
+function createPlayer(name, score = 0) {
+    return {
+        name,
+        score
+    }
+}
+turnClass()
+
 function init() {              //מאתחל את הקוד את הלוח 
     shuffle(cards); //  ועושה ערבוב קוראת למערך
     const board = document.getElementById("board"); // קורא לאלמנט
-    for (i in cards) {// עבור כל קלף ,כך תא קוראת לפונקציה של create card
+    for (i in cards) {// עבור כל קלף כל תא קורא לפונקציה של create card
         const element = createCard(i)
         board.appendChild(element)
     }
+    player1.innerText = `name:${players[0].name} score: ${players[0].score} `
+    player2.innerText = `name:${players[1].name} score: ${players[1].score} `
 
 }
 init();//קריאה לפנוקציה והקלפים  עולים
 
-function createCard(idx) {//  שזה בעצם עבור כל קלףמכניסים את האינדקס 
+function createCard(idx) {//  שזה בעצם עבור כל קלף מכניסים את האינדקס 
     const cardEl = document.createElement("div")
-    cardEl.innerHTML='<img src=" >'
+    cardEl.id = idx;
+    cardEl.innerHTML = '  <img src="./images.jpg" height="180" width="150">';
     cardEl.style.cursor = "pointer";
     // cardEl.innerHTML=cards[idx];// זה התוכן של הקלף (אימוג'י)
-    cardEl.addEventListener("click", element => cardEl.innerHTML = cards[idx]); // מראה של התוכן
-    cardEl.id = idx// ID של כל קלף
-    cardEl.className = "cards col-6 col-md-4 col-lg-2", // פריסה של הקלפים על המסך 
+    cardEl.addEventListener("click", e => {
+        // מראה של התוכן
+        cnt++
+        if (cnt <= 2) {
+            cardEl.innerHTML = cards[idx];
+            openCards.push(cardEl.id)
+        }
+        if (cnt === 2) {
+            console.log(players[turn++].name);
 
+            if (turn === players.length) {
+                turn = 0;
+            }
+            if (cards[openCards[0]] === cards[openCards[1]]) {
+                turn--
+                if (turn < 0) {
+                    turn = players.length - 1
+                }
+                players[turn].score += 1
+
+                setTimeout(() => {
+                    // alert("very good!!!")
+                    cnt = 0
+                    openCards = []
+                }, 500)
+                removeEventListener
+
+                console.log(players[turn].score);
+
+            }
+            else {
+                setTimeout(() => {
+                    // alert("try again!");
+                    document.getElementById(openCards[0]).innerHTML = '<img src="./images.jpg" height="180" width="150">';
+                    document.getElementById(openCards[1]).innerHTML = '<img src="./images.jpg" height="180" width="150">';
+                    openCards = []
+                    cnt = 0
+                }, 2000)
+            }
+        }
+
+        // ולשים את זה גם בהתחלה להפוך את זה לפוקנציה
+
+        turnClass();
+
+        player1.innerText = `name:${players[0].name} score: ${players[0].score} `
+        player2.innerText = `name:${players[1].name} score: ${players[1].score} `
+    });
+
+
+    cardEl.className = "cards col-6 col-md-4 col-lg-2"; // פריסה של הקלפים על המסך 
     return cardEl
 }
 
@@ -27,6 +106,15 @@ function shuffle(arr) {
     arr.sort(() => Math.random() - 0.5);
 }
 
-
+function turnClass() {
+    if (turn === 0) {
+        player1.className = "turn"
+        player2.className = ""
+    }
+    else {
+        player1.className = ""
+        player2.className = "turn"
+    }
+}
 
 
